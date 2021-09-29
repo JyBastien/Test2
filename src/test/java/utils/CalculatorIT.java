9c8f3c2202762f161861cc50855d66d7b4d9233a
JyBastien <77074544+JyBastien@users.noreply.github.com>
2021-09-29 12:07:20 -0400

    added test

---
+++ src/test/java/utils/CalculatorIT.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ThinkCenter
+ */
+public class CalculatorIT {
+    
+    public CalculatorIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class Calculator.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        int x = 2;
+        int y = 4;
+        Calculator instance = new Calculator();
+        int expResult = 5;
+        int result = instance.add(x, y);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+    
+}
